e661bb0941ad2c1a5fbf5009b0b1e8dd7a9f975d
nguyentiencuong21101999 <nguyentiencuong21101999>
2021-01-07 21:19:50 +0700

    a

--- controller/user/login.controller.js
+++ controller/user/login.controller.js
@@ -2,10 +2,12 @@
 const Register = require('../../models/web/register')
 
 module.exports.login = (req, res) => {
+    console.log(req.body);
     console.log(req.body.username);
     var username = req.body.username;
     var password = req.body.password;
     Register.findOne({ username: username, password: password }, (err, data) => {
+        console.log(data);
         res.send(data)
     })
 
